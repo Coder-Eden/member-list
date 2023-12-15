7683792b09e52e5dc78506aa3be3d12bf9b76db0
taikicoco <kawasakitaiki0308@gmail.com>
2023-12-15 15:24:26 +0900

    Replace Python with Go

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// GitHubUser は GitHub ユーザー情報を格納する構造体です。
+type GitHubUser struct {
+	Login     string `json:"login"`
+	Name      string `json:"name"`
+	AvatarURL string `json:"avatar_url"`
+}
+
+// Member はメンバー情報を格納する構造体です。
+type Member struct {
+	GitHub        string
+	InvitedPerson string
+	InvitedBy1    string
+	InvitedBy2    string
+	InvitedBy3    string
+}
+
+// getGitHubUserInfo は GitHub API を使用してユーザー情報を取得します。
+func getGitHubUserInfo(username string) (*GitHubUser, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s", username)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status code: %d", resp.StatusCode)
+	}
+
+	var user GitHubUser
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// createMarkdownLink は Markdown 形式のリンクを生成します。
+func createMarkdownLink(user *GitHubUser) string {
+	name := user.Name
+	if name == "" {
+		name = user.Login
+	}
+	return fmt.Sprintf("[%s](https://github.com/%s)", name, user.Login)
+}
+
+// createAvatarLink はアバターの画像リンクを生成します。
+func createAvatarLink(user *GitHubUser) string {
+	return fmt.Sprintf("![image](%s)", user.AvatarURL)
+}
+
+// createMarkdownTable はメンバーのリストから Markdown テーブルを生成します。
+func createMarkdownTable(members []Member) (string, error) {
+	var builder strings.Builder
+	builder.WriteString("| Number | ユーザー（GitHub） | アバター | 招待された人 | 招待した人（1）| 招待した人（2）| 招待した人（3）| 招待数 |\n")
+	builder.WriteString("|-------|------------------|--------|------------|----------------|----------------|----------------|------|\n")
+
+	for i, member := range members {
+		user, err := getGitHubUserInfo(member.GitHub)
+		if err != nil {
+			return "", err
+		}
+		markdownLink := createMarkdownLink(user)
+		avatarLink := createAvatarLink(user)
+		invitationCount := 0
+		for _, invite := range []string{member.InvitedBy1, member.InvitedBy2, member.InvitedBy3} {
+			if invite != "" {
+				invitationCount++
+			}
+		}
+
+		builder.WriteString(fmt.Sprintf("| %d | %s | %s | [%s](https://github.com/%s) | %s | %s | %s | %d |\n",
+			i+1, markdownLink, avatarLink, member.InvitedPerson, member.InvitedPerson,
+			member.InvitedBy1, member.InvitedBy2, member.InvitedBy3, invitationCount))
+	}
+
+	return builder.String(), nil
+}
+
+func main() {
+	// ファイルからメンバー情報を読み込む
+	data, err := ioutil.ReadFile("members.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var members []Member
+	err = json.Unmarshal(data, &members)
+	if err != nil {
+		panic(err)
+	}
+
+	markdownTable, err := createMarkdownTable(members)
+	if err != nil {
+		panic(err)
+	}
+
+	markdown := "## 概要\n\n![image](https://github.com/Coder-Eden/.github-private/assets/83957178/50505e63-2fba-4733-b825-b9b7e3615ad0)\n\n" +
+		"#### CODE EDENは25卒限定の「完全招待制」のオンラインコミュニティです。\n\n" +
+		"### 参加メンバー\n\n" + 
+		"以下の表は、招待された人、ユーザーのGitHubプロフィール、何人目に招待されたか、および各ユーザーによって招待された人のリストを示しています。\n\n" + 
+		markdownTable
+
+	// 結果を Markdown ファイルに書き出す
+	err = ioutil.WriteFile("README.md", []byte(markdown), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
