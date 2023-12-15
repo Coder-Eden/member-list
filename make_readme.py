83a1970e398acaca892b85d71afac2ce3f3b1c05
taikicoco <kawasakitaiki0308@gmail.com>
2023-12-15 13:22:52 +0900

    clean

--- make_readme.py
+++ make_readme.py
@@ -4,42 +4,46 @@ import requests
 def get_github_user_info(username):
     response = requests.get(f"https://api.github.com/users/{username}")
     if response.status_code == 200:
-        user_data = response.json()
-        return user_data["name"], user_data["avatar_url"]  # 実名とアバターURLを返す
+        return response.json()
     else:
         print(f"ユーザー情報の取得に失敗しました。ステータスコード: {response.status_code}")
-        return None, None
+        return None
 
-# JSONファイルを読み込む
-with open('members.json', 'r', encoding='utf-8') as file:
-    members = json.load(file)
+def create_markdown_link(username, user_data):
+    real_name = user_data["name"] if user_data["name"] else username
+    github_link = f"https://github.com/{username}"
+    return f"[{real_name}]({github_link})"
 
-markdown_table = "## 概要\n\n"
-markdown_table += "![image](https://github.com/Coder-Eden/.github-private/assets/83957178/50505e63-2fba-4733-b825-b9b7e3615ad0)\n\n"
-markdown_table += "#### CODE EDENは25卒限定の「完全招待制」のオンラインコミュニティです。\n\n"
-markdown_table += "### 参加メンバー\n\n"
-markdown_table += "| Number | ユーザー（GitHub） | アバター | 招待された人 | 招待した人（1）| 招待した人（2）| 招待した人（3）| 招待数 |\n"
-markdown_table += "|-------|------------|------|------|----------------|----------------|----------------|------|\n"
+def create_avatar_link(user_data):
+    return f"![image]({user_data['avatar_url']})" if user_data else "![image](default_avatar_url)"
 
-for index, member in enumerate(members, start=1):
-    github_username = member['github']
-    real_name, avatar_url = get_github_user_info(github_username)  # 実名とアバターURLを取得
-    github_link = f"https://github.com/{github_username}"
-    
-    if not real_name:
-        real_name = github_username
+def generate_markdown_table(members):
+    markdown_table = "| Number | ユーザー（GitHub） | アバター | 招待された人 | 招待した人（1）| 招待した人（2）| 招待した人（3）| 招待数 |\n"
+    markdown_table += "|-------|------------------|--------|------------|----------------|----------------|----------------|------|\n"
 
-    markdown_link = f"[{real_name}]({github_link})"
-    avatar_link = f"![image]({avatar_url})"
+    for index, member in enumerate(members, start=1):
+        user_data = get_github_user_info(member['github'])
+        markdown_link = create_markdown_link(member['github'], user_data)
+        avatar_link = create_avatar_link(user_data)
+        invited_person_link = f"[{member['invited_person']}](https://github.com/{member['invited_person']})"
+        invitation_count = sum(inv != "null" for inv in [member['invited_by_1'], member['invited_by_2'], member['invited_by_3']])
 
-    # 招待された人のGitHubリンク
-    invited_person_username = member['invited_person']
-    invited_person_link = f"[{invited_person_username}](https://github.com/{invited_person_username})"
+        markdown_table += f"| {index} | {markdown_link} | {avatar_link} | {invited_person_link} | {member['invited_by_1']} | {member['invited_by_2']} | {member['invited_by_3']} | {invitation_count} |\n"
 
-    invitation_count = len([inv for inv in [member['invited_by_1'], member['invited_by_2'], member['invited_by_3']] if inv != "null"])
+    return markdown_table
 
-    markdown_table += f"| {index} | {markdown_link} | {avatar_link} | {invited_person_link} | {member['invited_by_1']} | {member['invited_by_2']} | {member['invited_by_3']} | {invitation_count} |\n"
+def main():
+    with open('members.json', 'r', encoding='utf-8') as file:
+        members = json.load(file)
 
-# README.mdファイルにMarkdown表を書き込む
-with open('README.md', 'w', encoding='utf-8') as file:
-    file.write(markdown_table)
+    markdown = "## 概要\n\n![image](https://github.com/Coder-Eden/.github-private/assets/83957178/50505e63-2fba-4733-b825-b9b7e3615ad0)\n\n"
+    markdown += "#### CODE EDENは25卒限定の「完全招待制」のオンラインコミュニティです。\n\n"
+    markdown += "### 参加メンバー\n\n" 
+    markdown += "以下の表は、招待された人、ユーザーのGitHubプロフィール、何人目に招待されたか、および各ユーザーによって招待された人のリストを示しています。\n\n"
+    markdown += generate_markdown_table(members)
+
+    with open('README.md', 'w', encoding='utf-8') as file:
+        file.write(markdown)
+
+if __name__ == "__main__":
+    main()
